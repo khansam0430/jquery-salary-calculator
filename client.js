4fafeed74ac347a56248e0a45df56ec6c8633d1e
Samreen Khan <khansam0430@gmail.com>
2020-02-02 16:58:01 -0600

    working monthly cost color changer

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ $( document ).ready(readyNow);
 
 //globals
 let employeeList = [];
+let monthlyCost= 0;
 
 function readyNow(){
     console.log('in readyNow');
@@ -30,31 +31,48 @@ function submit() {
     $( '#idIn' ).val('');
     $( '#titleIn' ).val('');
     $( '#annualSalaryIn' ).val('');
-    //clear the inputs with .val. Use ; to end statement instead of ,
+    //clear the inputs with .val. Use ; to end statement instead of , 
+    monthlyCostCalculator();
+    //create variables to add up total monthly cost
+    let el= $('#monthlyCost');
+    el.empty();
+    el.append(`${monthlyCost}`);
+
 }//end submit
 
 function displayEmployeeList() {
     console.log('in displayEmployeeList');
     //console log to see if this is working
-    let el= $('#employeesTable');
+    let el= $('#employeeInfo');
     el.empty();
     //select and empty output element
     //loop through array
-    for (employee of employeeList){
+    for (let i=0; i<employeeList.length; i++){
         el.append(`
             <tr>
-                <td>${employee.firstName}</td>
-                <td>${employee.lastName}</td>
-                <td class="id-number">${employee.id}</td>
-                <td>${employee.title}</td>
-                <td>$${employee.annualSalary}</td>
+                <td>${employeeList[i].firstName}</td>
+                <td>${employeeList[i].lastName}</td>
+                <td class="id-number">${employeeList[i].id}</td>
+                <td>${employeeList[i].title}</td>
+                <td>$${employeeList[i].annualSalary}</td>
                 <td><button class="deleteButton">Delete</button></td>
             </tr>
         `)   //append inputs to table, add delete button after every employee row
     }//end for loop
     
-    
 }// end displayEmployeeList
+
+function monthlyCostCalculator() {
+    console.log('in monthlyCostCalculator');
+    for (let i = 0; i < employeeList.length; i++) {
+        monthlyCost += employeeList[i].annualSalary / 12;
+    //have monthlyCost add up annualSalary, then have annualSalary divide 12 for months in year
+    }//end for loop
+    if (monthlyCost > 20000) {
+        //document.getElementById("totalMonthlyCost").style.backgroundColor = "red";
+        $('#totalMonthlyCost').css({"color":"red"});
+    }
+}//end monthlyCostCalculator
 
 function onDeleteButton() {
     console.log('onDeleteButton');
