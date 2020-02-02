83c57982fa5e125ae16e04024457aee80cbe1fcc
Samreen Khan <khansam0430@gmail.com>
2020-02-02 13:35:53 -0600

    sucessfully displaying on DOM

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 $( document ).ready(readyNow);
 
+//globals
+let employeeList = [];
+
 function readyNow(){
     console.log('in readyNow');
     $( '#submitButton').on('click',submit);
@@ -15,8 +18,28 @@ function submit() {
         id: $( '#idIn' ).val(),
         title: $( '#titleIn' ).val(),
         annualSalary: $( '#annualSalaryIn' ).val(),
-    }// newObject
+    }// end newObject
     console.log('adding:', newObject);
-    
+    employeeList.push(newObject);
+    //push newObject into employeeList array
+    displayEmployeeList();
+    //show the employeeList
 
 }//end submit
+
+function displayEmployeeList() {
+    console.log('in displayEmployeeList');
+    //select and empty output element
+    let el= $('#employeesOut');
+    el.empty();
+    //loop through array
+    for (let i = 0; i < employeeList.length; i++) {
+        //append each employee to the DOM
+        el.append(`<li>${employeeList[i].firstName} ${employeeList[i].lastName} 
+        ${employeeList[i].id} ${employeeList[i].title} ${employeeList[i].annualSalary}
+        </li>`);
+    }//end for loop
+    
+    
+    
+}// end displayEmployeeList
