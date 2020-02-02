f971d047974ec57c3c016c0905c6496230bc2afb
Samreen Khan <khansam0430@gmail.com>
2020-02-02 12:26:56 -0600

    user inputs collected

--- client.js
+++ client.js
@@ -1,6 +1,22 @@
 $( document ).ready(readyNow);
 
 function readyNow(){
-    console.log('hihi');
+    console.log('in readyNow');
+    $( '#submitButton').on('click',submit);
+
+}//end readyNow
+
+function submit() {
+    console.log('in addItem');
+    //get user input and create object
+    let newObject= {
+        firstName: $( '#firstNameIn' ).val(),
+        lastName: $( '#lastNameIn' ).val(),
+        id: $( '#idIn' ).val(),
+        title: $( '#titleIn' ).val(),
+        annualSalary: $( '#annualSalaryIn' ).val(),
+    }// newObject
+    console.log('adding:', newObject);
     
-}
+
+}//end submit
